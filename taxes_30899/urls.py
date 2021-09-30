7f6a9e3731605d7be60e047c2bc7605aca5e6703
Crowdbotics <team@crowdbotics.com>
2021-09-30 22:13:35 +0000

    Update admin title

--- taxes_30899/urls.py
+++ taxes_30899/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Taxes"
-admin.site.site_title = "Taxes Admin Portal"
-admin.site.index_title = "Taxes Admin"
+admin.site.site_header = "test"
+admin.site.site_title = "test Admin Portal"
+admin.site.index_title = "test Admin"
 
 # swagger
 api_info = openapi.Info(
